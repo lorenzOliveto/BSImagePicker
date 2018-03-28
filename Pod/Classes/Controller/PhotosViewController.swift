99df7f791e730f1d9df8821956af0833cd5fb393
LorenzOliveto <lorenzo.oliveto@mumbleideas.it>
2018-03-28 09:38:31 +0200

    Edited max image count message

--- Pod/Classes/Controller/PhotosViewController.swift
+++ Pod/Classes/Controller/PhotosViewController.swift
@@ -355,7 +355,7 @@ extension PhotosViewController {
             }
         }
         else if photosDataSource.selections.count >= settings.maxNumberOfSelections {
-            let alertController = UIAlertController(title: "Cen select a maximum of \(settings.maxNumberOfSelections) images", message: nil, preferredStyle: .alert)
+            let alertController = UIAlertController(title: "Select up to \(settings.maxNumberOfSelections) images.", message: nil, preferredStyle: .alert)
             alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
             self.present(alertController, animated: true, completion: nil)
         }
